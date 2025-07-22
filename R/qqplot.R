6e2aba896f7e7cecadc4e2123ed31eb82cb066f7
Willis <twil15@mlsle16005.childrens.sea.kids>
2025-07-22 13:14:11 -0700

    Added geom_point option for Q-Q plots

--- R/qqplot.R
+++ R/qqplot.R
@@ -2,14 +2,15 @@
 ##' @description Draws a Q-Q plot of p-values from a data.frame.
 ##' @param daf data.frame containing p-values.
 ##' @param p_col p-value column label
+##' @param geom character string specifying the type of geom to use for the Q-Q plot (default is "line")
 ##' @return ggplot object
 ##' @importFrom ggplot2 ggplot aes geom_abline labs coord_fixed stat_qq
 ##' @importFrom ggtext element_markdown
 ##' @importFrom stats qunif
 ##' @export
-qqplot <- function(daf, p_col) {
+qqplot <- function(daf, p_col, geom = "line") {
   ggplot2::ggplot(data = daf) +
-    ggplot2::stat_qq(ggplot2::aes(sample = !!rlang::sym(p_col)), geom = "line", distribution = stats::qunif) +
+    ggplot2::stat_qq(ggplot2::aes(sample = !!rlang::sym(p_col)), geom = geom, distribution = stats::qunif) +
     ggplot2::geom_abline(slope = 1, intercept = 0, color = "blue", linetype = "dashed") +
     scale_x_neglog10() +
     scale_y_neglog10() +
@@ -31,6 +32,7 @@ qqplot <- function(daf, p_col) {
 #' @param p_col p-value column label for primary variable
 #' @param q_col p-value column label for secondary variable for stratification
 #' @param breaks numeric vector of breaks for stratification
+#' @param geom character string specifying the type of geom to use for the Q-Q plot (default is "line")
 #' @return ggplot object
 #' @importFrom ggplot2 ggplot aes geom_abline labs coord_fixed stat_qq
 #' @importFrom stats qunif quantile
@@ -38,7 +40,7 @@ qqplot <- function(daf, p_col) {
 #' @importFrom stats qunif
 #' @importFrom dplyr mutate select
 #' @export
-stratified_qqplot <- function(daf, p_col, q_col, breaks = c(0, 10^(c(-5:-1, 0)))) {
+stratified_qqplot <- function(daf, p_col, q_col, breaks = c(0, 10^(c(-5:-1, 0))), geom = "line") {
   tibble::as_tibble(daf) %>%
     dplyr::mutate(
       qbreaks = list(stats::quantile(!!rlang::sym(q_col), probs = breaks, na.rm = TRUE)),
@@ -50,7 +52,7 @@ stratified_qqplot <- function(daf, p_col, q_col, breaks = c(0, 10^(c(-5:-1, 0)))
     ) %>%
     dplyr::select(-qbreaks) %>%
     ggplot2::ggplot() +
-    ggplot2::stat_qq(ggplot2::aes(sample = !!rlang::sym(p_col), col = Q), geom = "line", distribution = stats::qunif) +
+    ggplot2::stat_qq(ggplot2::aes(sample = !!rlang::sym(p_col), col = Q), geom = geom, distribution = stats::qunif) +
     ggplot2::geom_abline(slope = 1, intercept = 0, color = "blue", linetype = "dashed") +
     scale_x_neglog10() +
     scale_y_neglog10() +
--- tests/testthat/test-qqplot.R
+++ tests/testthat/test-qqplot.R
@@ -12,6 +12,21 @@ test_that("qqplot matches expected output visually", {
   )
 })
 
+test_that("qqplot matches expected output visually with geom_point", {
+  set.seed(42)
+  vdiffr::expect_doppelganger(
+    "qqplot example with geom_point",
+    qqplot(
+      data.table::data.table(
+        SNP = paste0("rs", 1:100),
+        P = runif(100, 0, 1)
+      ),
+      p_col = "P",
+      geom = "point"
+    )
+  )
+})
+
 test_that("Stratified qqplot matches expected output visually", {
   set.seed(43)
   daf <- data.frame(p1 = c(runif(100, 0, 1e-3), runif(900, 0, 1)),
@@ -25,6 +40,23 @@ test_that("Stratified qqplot matches expected output visually", {
       p_col = "p1",
       q_col = "p2"
     )
+  )
+})
+
+test_that("Stratified qqplot matches expected output visually with geom_point", {
+  set.seed(43)
+  daf <- data.frame(p1 = c(runif(100, 0, 1e-3), runif(900, 0, 1)),
+                    p2 = c(runif(100, 0, 1e-3), runif(900, 0, 1))
+             )
+
+  vdiffr::expect_doppelganger(
+    "stratified_qqplot example with geom_point",
+    stratified_qqplot(
+      daf,
+      p_col = "p1",
+      q_col = "p2",
+      geom = "point"
+    ),
   )
 })
 
